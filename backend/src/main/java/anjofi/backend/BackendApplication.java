13f0a5ce17e77f347edfb24f2546521139be4050
João Pedro Menegali Salvan Bitencourt <joao.ms@aluno.ifsc.edu.br>
2023-06-22 17:14:15 -0300

    Ajustada a integração do backend com o servidor MQTT

--- backend/src/main/java/anjofi/backend/BackendApplication.java
+++ backend/src/main/java/anjofi/backend/BackendApplication.java
@@ -1,14 +1,17 @@
 package anjofi.backend;
 
+import org.eclipse.paho.client.mqttv3.MqttException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import anjofi.backend.controller.Iniciar;
+
+// import anjofi.backend.controller.Iniciar;
 
 @SpringBootApplication
 public class BackendApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws MqttException {
 		SpringApplication.run(BackendApplication.class, args);
-		
 	}
-
 }
--- backend/src/main/java/anjofi/backend/controller/Iniciar.java
+++ backend/src/main/java/anjofi/backend/controller/Iniciar.java
@@ -1,57 +1,23 @@
 package anjofi.backend.controller;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import anjofi.backend.AnjofiMqtt;
-
-
 @RestController
-
-@RequestMapping("/iniciar")
-
 public class Iniciar {
+    private AnjofiMqtt mqtt;
 
-    @GetMapping
-    public HashMap<String, String> iniciar() throws MqttException{
-            
+    public Iniciar () throws MqttException {
         AnjofiMqtt novo = new AnjofiMqtt("tcp://jpmsb.ddns.net", 51439, "anjofi","pji29006","anjofi" );
-        
-        novo.listen("AnJoFi/#");
-        
-        
-        String tratar;
-        ArrayList<Arduino> lista = new ArrayList<>();
-        tratar = novo.getTopicAndMessage();
-
-        tratar = novo.getLastReceivedData();
-
-        Arduino arduino = new Arduino();
-
-        
-        String[] substrings = tratar.split(",");
-
-        arduino.temperatura=substrings[0];
-        arduino.umidade = substrings[1];
-        arduino.estado = substrings[2];
-        arduino.lampada= substrings[3];
-
-        lista.add(arduino);
-
-
-        //novo.getLastDevice();
-        Operacao.iniciar();
-        return arduino.statusDispositivo();
+        this.mqtt = novo;
+        this.mqtt.listen("AnJoFi/#");
     }
 
     @Override
+    @GetMapping("/iniciar")
     public String toString() {
-        return "Iniciar []";
+        return mqtt.getLastReceivedData();
     }
-    
 }
