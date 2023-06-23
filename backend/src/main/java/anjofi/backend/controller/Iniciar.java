516248e46251ded2cabdcaaca2611204d056e603
João Pedro Menegali Salvan Bitencourt <joao.ms@aluno.ifsc.edu.br>
2023-06-23 20:24:12 -0300

    O identificador do cliente MQTT agora é o nome da máquina onde o mesmo está rodando

--- backend/src/main/java/anjofi/backend/controller/Iniciar.java
+++ backend/src/main/java/anjofi/backend/controller/Iniciar.java
@@ -3,16 +3,20 @@ package anjofi.backend.controller;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 import anjofi.backend.AnjofiMqtt;
+
 @RestController
 public class Iniciar {
     private AnjofiMqtt mqtt;
 
-    public Iniciar () throws MqttException {
-        AnjofiMqtt novo = new AnjofiMqtt("tcp://jpmsb.ddns.net", 51439, "anjofi","pji29006","anjofi" );
+    public Iniciar () throws MqttException, UnknownHostException {
+        AnjofiMqtt novo = new AnjofiMqtt("tcp://jpmsb.ddns.net", 51439, "anjofi","pji29006", InetAddress.getLocalHost().getHostName());
         this.mqtt = novo;
         this.mqtt.listen("AnJoFi/#");
+        System.out.println("iniciou mqtt");
     }
 
     @Override
