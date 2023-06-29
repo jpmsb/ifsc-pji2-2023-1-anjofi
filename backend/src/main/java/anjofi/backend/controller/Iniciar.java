4232d70f8fd08bfd16ebaeede6a5cee5b02b1954
João Pedro Menegali Salvan Bitencourt <joao.ms@aluno.ifsc.edu.br>
2023-06-29 15:14:15 -0300

    Ajuste para que o servidor não falhe ao iniciar caso o servidor MQTT não esteja disponível

--- backend/src/main/java/anjofi/backend/controller/Iniciar.java
+++ backend/src/main/java/anjofi/backend/controller/Iniciar.java
@@ -11,12 +11,29 @@ import anjofi.backend.AnjofiMqtt;
 @RestController
 public class Iniciar {
     private AnjofiMqtt mqtt;
+    private boolean conectouAoMQTT = false;
 
-    public Iniciar () throws MqttException, UnknownHostException {
-        AnjofiMqtt novo = new AnjofiMqtt("tcp://jpmsb.ddns.net", 51439, "anjofi","pji29006", InetAddress.getLocalHost().getHostName());
-        this.mqtt = novo;
-        this.mqtt.listen("AnJoFi/#");
-        System.out.println("iniciou mqtt");
+    public Iniciar () throws UnknownHostException, InterruptedException {
+        conectar();
+    }
+
+    public void conectar() throws UnknownHostException, InterruptedException {
+        AnjofiMqtt novo;
+
+        while (! this.conectouAoMQTT) {
+            try {
+                novo = new AnjofiMqtt("tcp://jpmsb.ddns.net", 51439, "anjofi","pji29006", InetAddress.getLocalHost().getHostName());
+
+                this.mqtt = novo;
+                this.mqtt.listen("AnJoFi/#");
+                this.conectouAoMQTT = true;
+                System.out.println("iniciou mqtt");
+            } catch (MqttException e) {
+                System.out.println("\n\nNão foi possível iniciar o cliente MQTT. Veja o erro abaixo:\n");
+                e.printStackTrace();
+                Thread.sleep(2000);
+            }
+        }
     }
 
     @Override
