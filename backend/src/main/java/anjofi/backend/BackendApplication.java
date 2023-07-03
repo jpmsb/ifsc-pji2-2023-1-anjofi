86d1b18ac86faed5a2790caecd2a60783b940444
João Pedro Menegali Salvan Bitencourt <joao.ms@aluno.ifsc.edu.br>
2023-07-03 11:26:30 -0300

    Incluída a lógica para que os valores de servidor, porta, usuário e senha, definidos no arquivo application.properties, possam ser sobrescritos através de argumentos de linha de comando.

--- backend/src/main/java/anjofi/backend/BackendApplication.java
+++ backend/src/main/java/anjofi/backend/BackendApplication.java
@@ -1,17 +1,47 @@
 package anjofi.backend;
 
-import org.eclipse.paho.client.mqttv3.MqttException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import anjofi.backend.controller.Iniciar;
-
-// import anjofi.backend.controller.Iniciar;
-
 @SpringBootApplication
 public class BackendApplication {
+	@Value("${mqtt.server}")
+	public static String mqttServer;
 
-	public static void main(String[] args) throws MqttException {
+	@Value("${mqtt.port}")
+	public static String mqttPort;
+
+	@Value("${mqtt.user}")
+	public static String mqttUser;
+
+	@Value("${mqtt.password}")
+	public static String mqttPassword;
+
+	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
+
+	public void run(ApplicationArguments args) {
+        if (args.containsOption("mqtt.server")) {
+            String mqttServerOverridden = args.getOptionValues("mqtt.server").get(0);
+            mqttServer = mqttServerOverridden;
+        }
+
+		if (args.containsOption("mqtt.port")) {
+            String mqttPortOverridden = args.getOptionValues("mqtt.port").get(0);
+            mqttPort = mqttPortOverridden;
+        }
+
+		if (args.containsOption("mqtt.user")) {
+            String mqttUserOverridden = args.getOptionValues("mqtt.user").get(0);
+            mqttUser = mqttUserOverridden;
+        }
+
+		if (args.containsOption("mqtt.password")) {
+            String mqttPasswordOverridden = args.getOptionValues("mqtt.password").get(0);
+            mqttPassword = mqttPasswordOverridden;
+        }
+    }
 }
