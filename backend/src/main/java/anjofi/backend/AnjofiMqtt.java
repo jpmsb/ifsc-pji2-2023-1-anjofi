efea169b7ad18b5f60b83f3f964c5aeb6beb219e
João Pedro Menegali Salvan Bitencourt <joao.ms@aluno.ifsc.edu.br>
2023-07-03 11:29:06 -0300

    Ajustado para que os dados coletados via MQTT sejam salvos em uma tabela hash, cuja chave é o ID do dispositivo e o valor é um vetor de String contendo cada JSON recebido.

--- backend/src/main/java/anjofi/backend/AnjofiMqtt.java
+++ backend/src/main/java/anjofi/backend/AnjofiMqtt.java
@@ -1,5 +1,7 @@
 package anjofi.backend;
 
+import java.util.HashMap;
+
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttClient;
@@ -13,13 +15,14 @@ public class AnjofiMqtt {
     private String identifier;
     private String lastReceivedData;
     private String lastDevice;
-    private String topicAndMessage;
     private MqttClient client;
+    private HashMap<String, String[]> receivedMessages;
 
     public AnjofiMqtt(String server, int port, String identifier) throws MqttException {
         this.server = server;
         this.port = port;
         this.identifier = identifier;
+        this.receivedMessages = new HashMap<>();
         
         client = new MqttClient(server + ":" + port, identifier);
 
@@ -33,6 +36,7 @@ public class AnjofiMqtt {
 
     public AnjofiMqtt(String server, int port, String user, String password, String identifier) throws MqttException {
         client = new MqttClient(server + ":" + port, identifier);
+        this.receivedMessages = new HashMap<>();
 
         MqttConnectOptions options = new MqttConnectOptions();
         options.setAutomaticReconnect(true);
@@ -50,7 +54,7 @@ public class AnjofiMqtt {
                 lastReceivedData = new String(message.getPayload());
                 String[] deviceID = topic.split("/");
                 lastDevice = deviceID[1];
-                topicAndMessage = lastDevice + ":" + lastReceivedData;
+                storeNewMessage(lastDevice, lastReceivedData);
             }
 
             @Override
@@ -63,15 +67,45 @@ public class AnjofiMqtt {
         });
     }
 
+    private void storeNewMessage(String deviceID, String message) {
+        if (receivedMessages.containsKey(deviceID)) {
+            int currentMessageArrayLenght = receivedMessages.get(deviceID).length;
+            String[] newMessageArray = new String[currentMessageArrayLenght + 1];
+
+            System.arraycopy(receivedMessages.get(deviceID), 0, newMessageArray, 0,currentMessageArrayLenght);
+            newMessageArray[currentMessageArrayLenght] = message;
+
+            receivedMessages.put(deviceID, newMessageArray);
+        } else {
+            String[] newMessageArray = {message};
+            receivedMessages.put(deviceID, newMessageArray);
+        }
+    }
+
     public String getLastReceivedData(){
         return lastReceivedData;
+    }
+
+    public String getLastReceivedData(String deviceID){
+        if (receivedMessages.containsKey(deviceID)) {
+            int position = receivedMessages.get(deviceID).length - 1;
+            return receivedMessages.get(deviceID)[position];
+        } else return "";
     }
 
     public String getLastDevice(){
         return lastDevice;
     }
 
-    public String getTopicAndMessage(){
-        return topicAndMessage;
+    public String getAllDevices(){
+        String allDevices = "[";
+
+        for (String device : receivedMessages.keySet()) {
+            allDevices += "\"" + device + "\", ";
+        }
+        allDevices = allDevices.substring(0, allDevices.length() - 2);
+        allDevices += "]";
+        
+        return allDevices;
     }
 }
