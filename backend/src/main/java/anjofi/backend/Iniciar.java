b9989f283b5ab5d3ead084f72175965cbe13beea
João Pedro Menegali Salvan Bitencourt <joao.ms@aluno.ifsc.edu.br>
2023-07-03 18:00:25 -0300

    Removida dependência desnecessária

--- backend/src/main/java/anjofi/backend/Iniciar.java
+++ backend/src/main/java/anjofi/backend/Iniciar.java
@@ -1,7 +1,6 @@
 package anjofi.backend;
 
 import org.eclipse.paho.client.mqttv3.MqttException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
